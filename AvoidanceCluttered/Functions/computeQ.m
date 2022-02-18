cd98c4ce90ef2ae85f5ea14cfcc14c82dc15a505
biagio-trimarchi <biagio.trimarchi2@unibo.it>
2022-02-18 10:40:38 +0100

    Implemented first avoidance algoritm

--- AvoidanceCluttered/Functions/computeQ.m
+++ AvoidanceCluttered/Functions/computeQ.m
@@ -3,7 +3,7 @@ function Q = computeQ(n)
     
     for k = 0:2*n
         for i = max(0, k-n):min(n,k)
-            Q(k-i+1, i+1, k+1) = binomial(n, i)*binomial(n, k-i)/binomial(2*n, k);
+            Q(k-i+1, i+1, k+1) = nchoosek(n, i)*nchoosek(n, k-i)/nchoosek(2*n, k);
         end
     end
 end
